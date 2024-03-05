e26ef3e606788bf74fdbc9a1f4bebde1295d1b58
alick gardiner <alickgardiner@alicks-Air.home>
2024-03-05 13:42:21 +0000

    satisfy linter

--- test/controllers/news_controller_test.rb
+++ test/controllers/news_controller_test.rb
@@ -10,12 +10,12 @@ class NewsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should render a news article successfully' do
-    get news_article_url, :params => { :article => 'https://news.google.com/rss/articles/'\
+    get news_article_url, params: { article: 'https://news.google.com/rss/articles/'\
     'CBMinQFodHRwczovL3d3dy50aGVndWFyZGlhbi5jb20vYnVzaW5lc3MvbGl2ZS8yMDI0L21hci8wNS9qZXJlbXk'\
     'taHVudC1mcmVlemUtZnVlbC1kdXR5LWJ1ZGdldC11ay1jYXItc2FsZXMtZmVicnVhcnktc2VydmljZXMtcmVjZXN'\
     'zaW9uLWJpdGNvaW4tZ29sZC1idXNpbmVzcy1saXZl0gGdAWh0dHBzOi8vYW1wLnRoZWd1YXJkaWFuLmNvbS9idXNp'\
     'bmVzcy9saXZlLzIwMjQvbWFyLzA1L2plcmVteS1odW50LWZyZWV6ZS1mdWVsLWR1dHktYnVkZ2V0LXVrLWNhci1zYWx'\
-    'lcy1mZWJydWFyeS1zZXJ2aWNlcy1yZWNlc3Npb24tYml0Y29pbi1nb2xkLWJ1c2luZXNzLWxpdmU?oc=5'}    
+    'lcy1mZWJydWFyeS1zZXJ2aWNlcy1yZWNlc3Npb24tYml0Y29pbi1nb2xkLWJ1c2luZXNzLWxpdmU?oc=5' }
     assert_response :success
     assert_match 'Rolling coverage of the latest', @response.body
   end
