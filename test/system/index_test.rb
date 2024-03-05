1d0ad4fffdaf5f55806a2c91e3442768d8b87cac
alick gardiner <alickgardiner@alicks-Air.home>
2024-03-05 19:49:44 +0000

    Add basic UI tests

---
+++ test/system/index_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+
+class IndexTest < ApplicationSystemTestCase
+  def setup
+    visit root_url
+  end
+  
+  test "visiting the index with the cookie set" do
+    page.driver.browser.manage.add_cookie(name: "lat", value: "5")
+    visit root_url
+    assert_selector "a", text: "Weather forecast"
+    assert_selector "a", text: "Map directions"
+    assert_selector "a", text: "News"
+    assert_selector "a", text: "Change settings"
+    page.assert_selector(:css, "a[href=\"/settings\"]")
+  end
+
+  test "visiting the index without the cookie set" do
+    page.driver.browser.manage.delete_cookie('lat')
+    visit root_url
+    assert_text "Change your settings:"
+  end
+end
---
+++ test/system/settings_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SettingsTest < ApplicationSystemTestCase
+  test "visiting the settings page" do
+    visit settings_url
+    assert_text "Change your settings:"
+
+    end
+end
