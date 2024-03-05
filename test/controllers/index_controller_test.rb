947a45db57bb08927685868281608ac488a3929b
alick gardiner <alickgardiner@alicks-Air.home>
2024-03-05 10:33:36 +0000

    Add initial tests

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class IndexControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should load the index page successfully when the cookie is set' do
+    get root_url, headers: {"COOKIE" => "lat=57;"}
+    assert_response :success
+    assert_match 'Weather forecast', @response.body
+    assert_match 'Map directions', @response.body
+    assert_match 'News', @response.body
+    assert_match 'Change settings', @response.body
+  end
+
+  test 'should redirect to settings when the cookie is not set' do
+    get root_url
+    assert_response :redirect
+    assert_redirected_to settings_url
+  end
 end
--- test/controllers/news_controller_test.rb
+++ test/controllers/news_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class NewsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should load the news index page successfully when cookie is set' do
+    get news_url, headers: {"COOKIE" => "country_code=gb;"}
+    assert_response :success
+    assert_match 'News headlines', @response.body
+  end
 end
