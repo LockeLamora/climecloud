314ba8d8369aa4c3317777097b6f1755d4a1848d
alick gardiner <alickgardiner@alicks-MacBook-Air.local>
2024-03-06 15:21:49 +0000

    Add news article test

--- test/system/news_test.rb
+++ test/system/news_test.rb
@@ -4,13 +4,13 @@ require 'application_system_test_case'
 
 class NewsTest < ApplicationSystemTestCase
   def setup
-    # WebMock.enable!
     stub_request(:get, /news.google.com/).to_return(body: file_fixture('news_response.xml').read)
   end
 
-  test 'visiting the index' do
+  test 'visiting the index and then an article' do
     visit news_url
-
-    assert_selector 'h1', text: 'News'
+    assert_text 'Budget 2024 live: Jeremy Hunt'
+    first('.news > ul > a').click
+    assert_text "We've already taken you through"
   end
 end
