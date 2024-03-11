db33447137e5c2f127551c8b6a3dfa4ab70fe6e0
alick gardiner <alickgardiner@alicks-MacBook-Air.local>
2024-03-11 22:13:47 +0000

    Configure logging to identify sites that refuse to connect or require additional parsing rules

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Climecloud
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # create a logger with a file as a logging target
+    config.logger = Logger.new('log/output.log')
+    # set the minimum log level
+    config.log_level = :warn
   end
 end
